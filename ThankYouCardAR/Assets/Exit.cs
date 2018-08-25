137c0d64b1fa33fc6dd2850ec591e9de4df8e2fd
AaronCur <C00202505@itcarlow.ie>
2018-08-25 16:33:42 +0100

    Added thank you card project

---
+++ ThankYouCardAR/Assets/Exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+
+	// Close the app
+	public void Quit () {
+
+        Application.Quit();
+	}
+}
